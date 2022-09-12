3663550b965147b8698bb7a55c67084dca47fb1a
Rebeca Divina Paula Carvalhedo <rebecadivinapc@gmail.com>
2022-09-11 23:22:01 -0300

    refatorando conexao

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,14 +1,9 @@
+var dbConnection = require('../../config/dbConnection');
+
 module.exports = function(app){
     app.get('/noticias', function(req,res){
 
-        var mysql = require('mysql');
-
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '1234',
-            database: 'portal_noticias'
-        });
+        var connection = dbConnection();
 
         connection.query("SELECT * FROM noticias", function(error, result){
             console.log(result);
