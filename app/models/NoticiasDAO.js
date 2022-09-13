d14654b9eafae62c1e6899842b79fbba959e8a02
Rebeca Divina Paula Carvalhedo <rebecadivinapc@gmail.com>
2022-09-12 22:19:58 -0300

    fim

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -3,11 +3,12 @@ function NoticiasDAO(connection){
 }
 
 NoticiasDAO.prototype.getNoticias = function(callback){
-    this._connection.query('SELECT * FROM noticias', callback);
+    this._connection.query('SELECT * FROM noticias ORDER BY data_criacao DESC', callback);
 }
 
-NoticiasDAO.prototype.getNoticia = function(callback){
-    this._connection.query('SELECT * FROM noticias WHERE id_noticia = 2', callback);
+NoticiasDAO.prototype.getNoticia = function(id_noticia, callback){
+    console.log(id_noticia.id_noticia);
+    this._connection.query('SELECT * FROM noticias WHERE id_noticia = ' + id_noticia.id_noticia, callback);
 }
 
 NoticiasDAO.prototype.salvarNoticia = function(noticia, callback){
